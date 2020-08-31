a920dbf08ab421a31315e6faddf16e571b29404a
yerowyn <yerowyn@gmail.com>
2020-08-31 12:51:32 +0600

    add basic user auth routes

--- server/server.js
+++ server/server.js
@@ -1,29 +1,38 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const PORT = process.env.PORT;
 const app = express();
+app.use(bodyParser.json());
 
-require('dotenv').config({
-	path: './config/index.env'
-})
+require("dotenv").config({
+  path: "./config/index.env",
+});
+const connectDB = require("./config/db");
+connectDB();
 
-app.use(express.urlencoded({extended: true}))
-app.use(cors('dev'))
-app.use(morgan())
+app.use(morgan("dev"));
+app.use(cors());
 
-app.get('/', (req, res) => {
-	res.send('send');
-})
+// routes
+app.use("/api/user/", require("./routes/auth.route"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors("dev"));
+app.use(morgan());
+
+app.get("/", (req, res) => {
+  res.send("send");
+});
 
 app.use((req, res) => {
-	res.status(404).json({
-		msg: "Page not found"
-	})
-})
+  res.status(404).json({
+    msg: "Page not found",
+  });
+});
 
-
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
